2685fd30a467669da93e1f94000d448d22b9acf6
doyoung95 <satpray73@gmail.com>
2023-10-28 15:33:18 +0900

    23.10.28 until문

---
+++ until.sh
@@ -0,0 +1,18 @@
+# until은 조건이 참일 때까지 반복
+#아래 예제는 뭔지 모르겠음;
+# until who | grep "$1" > /dev/null # 출력을 저장하지 않고 버림
+# do
+#   sleep 10
+# done
+
+# echo -e \\a # 시스템 비프음
+# echo "$1 has just logged in"
+# exit 0
+
+# i=0
+# until [ $i = 5 ]
+# do
+#   echo "$i"
+#   i=$(($i+1))
+# done
+# exit 0
