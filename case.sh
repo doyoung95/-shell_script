8d545cc2adea11d00a45524205fcca84bc0c8aad
doyoung95 <satpray73@gmail.com>
2023-10-28 15:47:12 +0900

    23.10.28 case문

---
+++ case.sh
@@ -0,0 +1,15 @@
+# # 케이스문
+# # pattern을 이용함
+# echo "is it morning?"
+# read user_answer
+
+# case $user_answer in
+#   Y|y|yes|Yes|YES )
+#     echo "Good morning";;
+#   [nN]* )
+#     echo "Good afternoon";;
+#   * )
+#     echo "??"
+#     exit 1;;
+# esac # 구문을 마칠 때는 구문 뒤집어서 쓰는 듯 변태같이
+# exit 0
